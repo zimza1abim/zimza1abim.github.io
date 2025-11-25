3956a54c851ac0d202a236a3a743ebefc603686b
finallycometolife <finallycometolife@gmail.com>
2025-11-25 15:50:49 +0900

    Fix: Correct sitemap domain and rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://zimza1abim.github.io",
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
